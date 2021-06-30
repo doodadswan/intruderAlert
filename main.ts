1aedfcd767f15cd6c0152613c5348935298317eb
doodadswan <doodadswan@hotmail.com>
2021-06-30 12:28:08 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,13 +3,13 @@ input.onButtonPressed(Button.A, function () {
     control.reset()
 })
 radio.onReceivedString(function (receivedString) {
-    music.playMelody("C5 C5 B B C5 C5 B B ", 200)
+    music.playMelody("C5 C5 B B C5 C5 B B ", 175)
     basic.showString("INTRUDER!")
 })
 basic.forever(function () {
     pressure = pins.analogReadPin(AnalogPin.P0)
     while (pressure > 500) {
-        music.playMelody("C5 C5 B B C5 C5 B B ", 200)
+        music.playMelody("C5 C5 B B C5 C5 B B ", 175)
         radio.sendString("INTRUDER!")
         basic.showString("INTRUDER!")
     }
